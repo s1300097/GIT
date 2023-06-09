2d3cd4f8af7d604a1068bda0de972d59d033a6ce
s1300097 <s1300097@u-aizu.ac.jp>
2023-06-09 20:02:03 +0900

    Won/lost message added

--- DiceGame.c
+++ DiceGame.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 int main(){
-  int num1, num2;
+  int num1, num2, total;
   srand(time(NULL));
   num1 = rand() % 6 + 1;
   num2 = rand() % 6 + 1;
-  printf("Rolling Dice,,,\nDie 1: %d\nDie 2: %d\nTotal value: %d\n",\
-	 num1, num2, num1 + num2);
+  total =  num1 + num2;
+  printf("Rolling Dice,,,\nDie 1: %d\nDie 2: %d\nTotal value: %d\nyou ",\
+	 num1, num2, total);
+  if(total >= 7) printf("Won!\n");
+  else printf("lose.\n");
   return 0;
 }
